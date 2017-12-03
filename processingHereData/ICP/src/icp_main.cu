bb0ab7e9c966d0c7388a5c25bab60d990002b017
Amol <amolvagad@gmail.com>
2017-12-03 11:40:35 -0600

    Updated Makefile and added necessary includes to main and kernel files

---
+++ processingHereData/ICP/src/icp_main.cu
@@ -0,0 +1,22 @@
+// HOST Code to compute ICP for localization 
+
+
+#ifndef _MATRIXMUL_KERNEL_H_
+#define _MATRIXMUL_KERNEL_H_
+#include <stdio.h>
+#include "ICP_Compute.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include<vector>
+#include<time.h>
+#include<sys/time.h>
+#include<ctime>
+#include "dlib/optimization/optimization.h"
+#include "dlib/optimization/find_optimal_parameters_abstract.h"
+#include "dlib/optimization/optimization_bobyqa.h"
+#include "dlib/optimization/find_optimal_parameters.h" 
+#include "octree_code/octree.h"
+#include "globals.h"
+extern "C"
